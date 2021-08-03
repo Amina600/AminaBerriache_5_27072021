a8b5690fcf90e470efe0d402587343cd1f69fc03
Amina600 <aminaharaoui19@gmail.com>
2021-08-03 16:18:53 +0200

    integration maquette page detail de produit.

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,6 +1,11 @@
 
 //chargement de la liste de produits
-fetch("http://localhost:3000/api/teddies")
+let url="http://localhost:3000/api/";
+let article= "furniture";
+loadUrl();
+
+function loadUrl(){
+fetch(url+article)
     //transforme le résultat en json
     .then(function(res) {
         if (res.ok) {
@@ -8,27 +13,50 @@ fetch("http://localhost:3000/api/teddies")
         }
     })
     .then(function(produits) {
+        
         afficherProduits(produits);
     })
     .catch(function(err) {
         alert("Impossible de charger les produits");
     });
-
+}
 //Affichage produits sur la page
+//$('#example').clear();
 function afficherProduits(produits) {
+    
     let data = [];
     for (let produit of produits) {
-        data.push([produit._id, produit.name, produit.imageUrl, produit.price, produit.description, produit.colors]);
+        data.push([produit._id, produit.name, produit.imageUrl, produit.price, produit.description]);
     }
+    
     $('#example').DataTable( {
         data: data,
+        "columnDefs": [
+            {
+                "targets": [ 0 ],
+                "visible": false,
+            }
+        ],
         columns: [
             { title: "id" },
             { title: "name" },
             { title: "image" },
             { title: "price" },
             { title: "description" },
-            { title: "colors" }
+           
         ]
     } );
 }
+function modifierArticle() {
+    var select_article = document.getElementById("article");
+    console.log(select_article.value);
+    
+    article = select_article.value;
+  
+   var table = $('#example').DataTable();
+ 
+table.clear();
+    
+}
+loadUrl();
+
